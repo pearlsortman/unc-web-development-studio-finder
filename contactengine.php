fbb43d14111e628aff25a5b64a991c13e6af0413
FAITH WALKER <faithcorinne@gmail.com>
2014-11-18 18:47:39 -0500

    v9.3
    Added PHP (kinda) for contact form, made contact form, working on search
    results and styling

---
+++ contactengine.php
@@ -0,0 +1,45 @@
+<?php
+
+$Space = Trim(stripslashes($_POST['space']);
+$EmailTo = "faithcorinne@gmail.com";
+$Subject = "New Space Hub Request";
+$Name = Trim(stripslashes($_POST['Name'])); 
+$Tel = Trim(stripslashes($_POST['Tel'])); 
+$Email = Trim(stripslashes($_POST['Email'])); 
+$Descript = Trim(stripslashes($_POST['Descript'])); 
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name of Space: "
+$Body .= $Space;
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Space Description: ";
+$Body .= $Descript;
+$Body .= "\n";
+
+// send email 
+$success = mail($EmailTo, $Subject, $Body, "From: <$Name>");
+
+// redirect to success page 
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
+}
+?>
---
+++ contactthanks.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<head>
+	<title>Far Out!</title>
+	
+	<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+
+	<div id="page-wrap">
+
+		<h1>Your message has been sent! Please allow 1-2 business days for a response, and check back soon to view your space's new profile!</h1><br />
+		
+		<p><a href="index.html">Back to Homepage</a></p>
+	
+	</div>
+
+</body>
+
+</html>
