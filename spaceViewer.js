3d00610112c74ec0cc484e7756d8e31fa675b139
PearlSortman <sortman@live.unc.edu>
2014-12-06 11:19:39 -0500

    ready for testing client side

--- spaceViewer.js
+++ spaceViewer.js
@@ -11,34 +11,93 @@ $(document).ready(function () {
 					load_space_item(space_ids[i]);
 				}
 			}
-		});
+		}
+	);
 
 
 	$('#search-bar').on('submit',
 		function (e) {
 			e.preventDefault();
+			$.ajax(url_base + "/space.php",
+			{
+				type: "POST",
+				dataType: "json",
+				data: $(this).serialize(),
+				success: function (space_json, status, jqXHR) {
+					var s = new Space(space_json);
+					load_space_item_byLocation(s.makeSearchResultDiv());	
+				},
+				error: function (jqXHR, status, error) {
+					alert(jqXHR.responseText);
+				}
+			});
 		}
-		)
+	);
 
-	$('#hacker').on('click')
+	$('#hacker').on('click',
+		function (e) {
+			e.preventDefault();
+			$.ajax(url_base + "/space.php",
+			{
+				type: "POST",
+				dataType: "json",
+				data: $(this).serialize(),
+				success: function (space_json, status, jqXHR) {
+					var s = new Space(space_json);
+					load_space_item_byType(s.makeSearchResultDiv());
+				},
+				error: function (jqXHR, status, error) {
+					alert(jqXHR.responseText);
+				}
+			});
+		}
+	);
 
-	$('#writing').on('click')
+/*	$('#writing').on('click')
 
 	$('#music').on('click')
 
 	$('#art').on('click')
 
-	$('#')
+	$('#coworking').on('click',
+		)
+
+	$('#maker').on('click',
+		)*/
 });
 
-var load_space_item = function (id) {
-	$.ajax(url_base + "/space.php/",
-		{	
-			type: "GET",
-			dataType: "json",
-			success: function(space_json, status, jqXHR) {
-				var s = new Space(space_json);
-				$('#results').append(s.makeSearchResultDiv());
-			}
-		});
+var load_space_item_byType = function (type) {
+	$.ajax(url_base + "/space.php/" + type,
+	{	
+		type: "GET",
+		dataType: "json",
+		success: function(space_json, status, jqXHR) {
+			var s = new Space(space_json);
+			$('#results').append(s.makeSearchResultDiv());
+		}
+	});
 };		
+
+var load_space_item_byLocation = function (city, state) {
+	$.ajax(url_base + "/space.php/" + city + "/" + state,
+	{
+		type: "GET",
+		dataType: "json",
+		success: function(space_json, status, jqXHR) {
+			var s = new Space(space_json);
+			$('#results').append(s.makeSearchResultDiv());
+		}
+	});
+};
+
+var load_space_item_byTypeAndLocation = function (type, city, state) {
+	$.ajax(url_base + "/space.php/" + type + "/" + city + "/" + state,
+	{
+		type: "GET",
+		dataType: "json",
+		success: function (space_json, status, jqXHR) {
+			var s = new Space(space_json);
+			$('#results').append(s.makeSearchResultDiv());
+		}
+	});
+};
