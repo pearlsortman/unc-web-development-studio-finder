5a19854c5658e38c127c9eaff6dc13b63af5a7d2
PearlSortman <sortman@live.unc.edu>
2014-12-05 20:27:04 -0500

    server-side 2

--- spaceViewer.js
+++ spaceViewer.js
@@ -3,7 +3,8 @@ var url_base = "http://wwwx.cs.unc.edu/Courses/comp426-f14/sortman/final";
 $(document).ready(function () {
 
 	$.ajax(url_base + "/space.php",
-		{type: "GET",
+		{
+			type: "GET",
 			dataType: "json",
 			success: function(space_ids, status, jqXHR) {
 				for (var i=0; i<space_ids.length; i++) {
